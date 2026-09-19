9c76c372e3eb9dcdd73b924939ac27a70512ca70
smian078 <amankawale007@gmail.com>
2026-09-19 15:45:10 +0530

    Add FileInfo class with builder and toMap method

---
+++ third_party/android_file_picker/android/src/main/kotlin/com/mr/flutter/plugin/filepicker/FileInfo.kt
@@ -0,0 +1,64 @@
+package com.mr.flutter.plugin.filepicker
+
+import android.net.Uri
+
+class FileInfo(
+    val path: String?,
+    val name: String?,
+    val uri: Uri?,
+    val size: Long,
+    val safHandle: java.util.HashMap<String, Any>? = null
+) {
+    class Builder {
+        private var path: String? = null
+        private var name: String? = null
+        private var uri: Uri? = null
+        private var size: Long = 0
+        private var safHandle: java.util.HashMap<String, Any>? = null
+
+        fun withPath(path: String?): Builder {
+            this.path = path
+            return this
+        }
+
+        fun withName(name: String?): Builder {
+            this.name = name
+            return this
+        }
+
+        fun withSize(size: Long): Builder {
+            this.size = size
+            return this
+        }
+
+        fun withUri(uri: Uri?): Builder {
+            this.uri = uri
+            return this
+        }
+
+        fun withSafHandle(safHandle: java.util.HashMap<String, Any>?): Builder {
+            this.safHandle = safHandle
+            return this
+        }
+
+        fun build(): FileInfo {
+            return FileInfo(
+                this.path,
+                this.name,
+                this.uri,
+                this.size,
+                this.safHandle
+            )
+        }
+    }
+
+    fun toMap(): HashMap<String, Any?> {
+        return hashMapOf<String, Any?>(
+            Pair("path", path),
+            Pair("name", name),
+            Pair("size", size),
+            Pair("identifier", uri.toString()),
+            Pair("safHandle", safHandle)
+        )
+    }
+}
