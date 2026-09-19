88dc1bc8f8053738634855ad5f42aeec4630eb10
smian078 <amankawale007@gmail.com>
2026-09-19 14:51:55 +0530

    Add files via upload

--- test/retry_test.dart
+++ test/retry_test.dart
@@ -4,18 +4,31 @@ import 'package:cystem/core/utils/retry.dart';
 void main() {
   test('retries transient failures and succeeds', () async {
     var tries = 0;
-    final value = await withRetry(action: () async {
-      tries++;
-      if (tries < 3) throw StateError('temporary');
-      return 42;
-    }, baseDelay: Duration.zero);
+    final value = await withRetry(
+      action: () async {
+        tries++;
+        if (tries < 3) throw StateError('temporary');
+        return 42;
+      },
+      baseDelay: Duration.zero,
+    );
     expect(value, 42);
     expect(tries, 3);
   });
 
   test('does not retry when predicate rejects', () async {
     var tries = 0;
-    expect(() => withRetry(action: () async { tries++; throw ArgumentError('bad'); }, baseDelay: Duration.zero, shouldRetry: (_) => false), throwsArgumentError);
+    expect(
+      () => withRetry(
+        action: () async {
+          tries++;
+          throw ArgumentError('bad');
+        },
+        baseDelay: Duration.zero,
+        shouldRetry: (_) => false,
+      ),
+      throwsArgumentError,
+    );
     expect(tries, 1);
   });
 }
