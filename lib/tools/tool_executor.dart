52ab5cfd6a55469d79b60a317fef5ed894e56855
smian078 <amankawale007@gmail.com>
2026-09-19 14:29:54 +0530

    Add files via upload

---
+++ lib/tools/tool_executor.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'tool_registry.dart';
+
+class ToolExecutor {
+  ToolExecutor(this.registry);
+  final ToolRegistry registry;
+
+  Future<String> execute(String name, String argumentsJson) async {
+    try {
+      if (name == 'cystem_tools') {
+        final wrapper = jsonDecode(argumentsJson);
+        if (wrapper is Map && wrapper['tool'] is String && wrapper['arguments'] is Map) {
+          return execute(wrapper['tool'] as String, jsonEncode(wrapper['arguments']));
+        }
+      }
+      final args = registry.validateInvocation(name, argumentsJson);
+      final result = await registry.byName(name)!.execute(args);
+      return jsonEncode({'ok': true, 'result': result});
+    } catch (error) {
+      return jsonEncode({'ok': false, 'error': error.toString()});
+    }
+  }
+}
